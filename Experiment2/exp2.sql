617c3e2c2c51e0b8a580163fcb24f2edfcc40d9a
ravi-ranjan-eng <singhraviranjan807@gmail.com>
2026-01-26 14:36:54 +0530

    Update exp2.sql

--- Experiment2/exp2.sql
+++ Experiment2/exp2.sql
@@ -1,36 +1,59 @@
-CREATE TABLE Students (
-    student_id INT,
-    name VARCHAR(50),
-    city VARCHAR(50),
-    percentage DECIMAL(5,2)
+---customer name, product, quantity, price, and order date.
+
+create table customer_orders(
+order_id serial primary key,
+customer_name varchar(20),
+product varchar(20),
+quantity int,
+price numeric(10,2),
+order_date date
 );
-INSERT INTO Students VALUES
-(1, 'Amit', 'Delhi', 96.5),
-(2, 'Riya', 'Mumbai', 94.2),
-(3, 'Rahul', 'Delhi', 97.8),
-(4, 'Sneha', 'Mumbai', 98.1),
-(5, 'Ankit', 'Chandigarh', 95.6),
-(6, 'Pooja', 'Delhi', 93.4),
-(7, 'Karan', 'Chandigarh', 96.2);
+
+insert into customer_orders(customer_name,product,quantity,price,order_date) values
+('Amit', 'Laptop', 1, 55000, '2025-01-05'),
+('Amit', 'Mouse', 2, 800, '2025-01-06'),
+('Riya', 'Mobile', 1, 22000, '2025-01-10'),
+('Riya', 'Headphones', 1, 2000, '2025-01-10'),
+('Karan', 'Laptop', 1, 60000, '2025-02-02'),
+('Karan', 'Keyboard', 1, 1500, '2025-02-05'),
+('Neha', 'Mobile', 2, 21000, '2025-02-15'),
+('Neha', 'Charger', 3, 900, '2025-02-18');
+
+select*from customer_orders;
+
+--Filtering Data Using Conditions Show only those customer who purcahse 20000 above show customer name 
+--and product and quantity and price
+
+select order_id,customer_name,product,quantity,price
+from customer_orders where price>20000;
 
 
----Without Case Statement
-SELECT CITY , COUNT(*) AS STUDET_COUNT FROM Students 
-WHERE percentage> 95
-GROUP BY city;
+---Sorting Query Results
+--ascending
+select order_id,customer_name,product,quantity,price
+from customer_orders where price>20000 order by price ;
 
--- WITH CASE STATEMENT
+--descending
+select order_id,customer_name,product,quantity,price
+from customer_orders where price>20000 order by price desc;
+
+--Grouping Data for Aggregation
+select product ,count(*)as total_product_sale
+from customer_orders
+group by product;
 
 
-SELECT CITY, SUM(CASE WHEN percentage> 95 THEN 1
-ELSE 0 END) AS STUDENT_COUNTS  FROM Students 
-GROUP BY city
+--Step 5: Applying Conditions on Aggregated Data
+select product,
+sum(quantity*price) as total_revenue
+from customer_orders
+group by product
+having sum(quantity*price) > 50000;
 
--- (II)
 
-
-
-SELECT CITY, AVG(CASE WHEN PERCENTAGE>95 THEN PERCENTAGE ELSE
-NULL END) AS STUDENT_AVG  FROM Students 
-GROUP BY city
-ORDER BY STUDENT_AVG DESC;
+--step6
+select product, sum(quantity*price) as total_revenue
+from customer_orders
+where order_date >= '2025-01-01'
+group by product
+having sum(quantity*price) > 50000;
